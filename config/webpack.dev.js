a955629480e2f83dba43185dc577c4bd60da510a
hmc <979701877@qq.com>
2022-11-19 10:27:36 +0800

    HMR

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -105,7 +105,8 @@ module.exports = {
     devServer: {
         host: "localhost",  //启动服务器域名
         port: "3000",   //启动服务器端口号
-        open: true  //是否自动打开浏览器
+        open: true,  //是否自动打开浏览器
+        hot: true   //开启HMR（默认值）
     },
     //模式 development production
     mode: 'development',
--- src/main.js
+++ src/main.js
@@ -9,3 +9,10 @@ import "./css/iconfont.css";
 
 console.log(count(2, 1));
 console.log(sum(1, 2, 3, 4));
+
+//css文件默认支持HMR，是因为style-loader实现了这个功能，js文件需要写如下代码才能实现HMR
+if (module.hot) {
+    //判断是否支持热模块替换功能
+    module.hot.accept("./js/count");
+    module.hot.accept("./js/sum");
+}
